07639e92a423297fe0a4ab8a24415134e2ed4498
liamquinn14 <138837924+liamquinn14@users.noreply.github.com>
2023-09-23 11:04:38 +0100

    can draw with multiple colours, including eraser

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+let colour = "black"
+
+let changeColour = (param) => {
+    colour = param
+    return colour
+}
+
 let generateGrid = () => {
 let container = document.getElementById('container')
-let div = document.createElement('div')
-div.classList.add('box')
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 15; j++) {
         let div = document.createElement('div')
         div.classList.add('box')
         div.addEventListener('mouseover', (e) => {
-            e.target.style.background = "pink" })
+            e.target.style.background = colour })
         container.appendChild(div)
         }
     } 
