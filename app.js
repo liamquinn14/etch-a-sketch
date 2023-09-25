d7c9a710c05b52c9e78484ef108970e1ca9bd0d9
liamquinn14 <138837924+liamquinn14@users.noreply.github.com>
2023-09-25 03:18:21 +0100

    adjustable canvas with 'create new canvas' option

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+let genButton = document.getElementById("generate-button")
+let genButtonHolder = document.getElementById("gen-button-holder")
 let colour
+
 function clearCanvas() {
     const boxes = document.querySelectorAll('.box');
     boxes.forEach((box) => {
@@ -15,21 +18,34 @@ let changeColour = (param) => {
 }
 
 let generateGrid = () => {
-let container = document.getElementById('container')
-for (let i = 0; i < 21; i++) {
-    for (let j = 0; j < 20; j++) {
+let gridSquares = window.prompt("How many pixels wide would you like your canvas? Enter a number between 1 and 100. The higher the number, the smaller the pen.")
+if (Number(gridSquares) > 100 || Number(gridSquares) < 1 ) {
+    alert("Sorry that is an invalid number of pixels. Please enter a number between 1 and 100.")
+    return
+} else {
+genButton.remove()
+genButtonHolder.remove()
+const container = document.createElement('div');
+document.body.appendChild(container);
+container.classList.add('container')
+container.setAttribute('id', 'container')
+let width = 580 / Number(gridSquares)
+let height = 580 / Number(gridSquares)
+for (let i = 0; i < gridSquares; i++) {
+    for (let j = 0; j < gridSquares; j++) {
         let div = document.createElement('div')
         div.classList.add('box')
+        div.style.width = width + "px"
+        div.style.height = height + "px"
         div.addEventListener('mouseover', (e) => {
             e.target.style.background = colour })
-        div.addEventListener('click', (e) => {
+        div.addEventListener('mousedown', (e) => {
             e.target.style.background = "whitesmoke" })
         container.appendChild(div)
         }
     } 
 }
-
-generateGrid()
+}
 
 let colourDisplay = document.getElementById('colour-display')
 
